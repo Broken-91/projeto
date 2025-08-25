657f072a1554eb669e6819b7d23b5dba87a2864f
PYETRO FERREIRA DE SOUZA <25205137@cfp127aluno.local>
2025-08-25 16:26:17 -0300

    Finalizada a aula de GIT

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 public static void main(String[] args){
 		System.out.println("Olá, Galera!!");
 		System.out.println("Estamos no SENAI!!");
+		System.out.println("E o GIT funciona");
+	
 	}
 }
